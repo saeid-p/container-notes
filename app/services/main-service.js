cf27416e89154b2289a347b0e36fccd0168a0e3d
saeid-p <pournejati@gmail.com>
2023-11-11 09:58:48 -0800

    Update test app.

--- app/services/main-service.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getMessage = void 0;
-const getMessage = () => "Hello world! Welcome!";
-exports.getMessage = getMessage;
-//# sourceMappingURL=main-service.js.map
